cf04d94bde1635d43828d4c12a81b9a8a10065f2
Alexander Schultheiß <aschulth@gmail.com>
2023-04-07 14:48:40 +0200

    Renaming functions and variables for better readability

--- dice.go
+++ dice.go
@@ -5,14 +5,14 @@ import (
 	"time"
 )
 
-func Roll(s int) int {
+func Roll(sides int) int {
 	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(s) + 1
+	return rand.Intn(sides) + 1
 }
 
-func RollPerc(percentage int) bool {
-    if percentage >= 100 { return true }
-    if percentage <= 0 { return false }
-    if Roll(100) <= percentage { return true }
+func RollChance(chance int) bool {
+    if chance >= 100 { return true }
+    if chance <= 0 { return false }
+    if Roll(100) <= chance { return true }
     return false
 }
